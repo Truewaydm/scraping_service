f85f779482485c3ee6945a349d67e26213fa9d0e
dpris <dmitry@snov.io>
2023-01-27 14:57:59 +0200

    03 002 search by parameters from the form

--- scraping/views.py
+++ scraping/views.py
@@ -5,6 +5,16 @@ from .models import Vacancy
 # Create your views here.
 
 def home_view(request):
-    print(request.POST)
-    query_set = Vacancy.objects.all()
+    # print(request.GET)
+    city = request.GET.get('city')
+    language = request.GET.get('language')
+    query_set = []
+    if city or language:
+        list_filter = {}
+        if city:
+            list_filter['city__name'] = city
+        if language:
+            list_filter['language__name'] = language
+    query_set = Vacancy.objects.filter(**list_filter)
+    # query_set = Vacancy.objects.all()
     return render(request, 'scraping/home.html', {'object_list': query_set})
