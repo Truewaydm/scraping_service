ce229c13e6acb1a5ff81ed45d9b42f986bf4f57f
dpris <dmitry@snov.io>
2023-01-28 12:34:35 +0200

    03 003 form as a built in class in Django

--- scraping/views.py
+++ scraping/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+
+from .forms import FindForm
 from .models import Vacancy
 
 
@@ -6,15 +8,16 @@ from .models import Vacancy
 
 def home_view(request):
     # print(request.GET)
+    form = FindForm()
     city = request.GET.get('city')
     language = request.GET.get('language')
     query_set = []
     if city or language:
         list_filter = {}
         if city:
-            list_filter['city__name'] = city
+            list_filter['city__slug'] = city
         if language:
-            list_filter['language__name'] = language
+            list_filter['language__slug'] = language
     query_set = Vacancy.objects.filter(**list_filter)
     # query_set = Vacancy.objects.all()
-    return render(request, 'scraping/home.html', {'object_list': query_set})
+    return render(request, 'scraping/home.html', {'object_list': query_set, 'form': form})
