50d2ac3b8c73909d830621e81fff48abc60bac9c
dpris <dmitry@snov.io>
2023-02-11 14:48:39 +0200

    07_006 data change deleting user

--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import authenticate, login, logout, get_user_model
 
 from accounts.forms import UserLoginForm, UserRegistrationForm, UserUpdateForm
 
-
 # Create your views here.
+
+User = get_user_model()
+
 
 def login_view(request):
     form = UserLoginForm(request.POST or None)
@@ -38,13 +40,28 @@ def update_view(request):
         user = request.user
         if request.method == 'POST':
             form = UserUpdateForm(request.POST)
-        else:
-            form = UserUpdateForm(
-                initial={
-                    'city': user.city,
-                    'language': user.language,
-                    'send_email': user.send_email
-                })
-            return render(request, 'accounts/update.html', {'form': form})
+            if form.is_valid():
+                data = form.cleaned_data
+                user.city = data['city']
+                user.language = data['language']
+                user.send_email = data['send_email']
+                user.save()
+                return redirect('accounts:update')
+        form = UserUpdateForm(
+            initial={
+                'city': user.city,
+                'language': user.language,
+                'send_email': user.send_email
+            })
+        return render(request, 'accounts/update.html', {'form': form})
     else:
         return redirect('accounts:login')
+
+
+def delete_view(request):
+    if request.user.is_authenticated:
+        user = request.user
+        if request.method == 'POST':
+            query_set = User.objects.get(pk=user.pk)
+            query_set.delete()
+    return redirect('home')
